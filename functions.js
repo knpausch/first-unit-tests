2b7ed8259ac4803b4d365511cef10fe526778548
Keyaanna (Kiko) Pausch <knpausch@gmail.com>
2022-08-25 11:03:21 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,9 +1,25 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+  var result = num1 + num2;
+  return result;
+}
+
+function sayHello(name) {
+var newGreet;
+
+  if(name === "Will")
+  {
+    newGreet = "No more testing Will!"
+  }
+  else {
+    newGreet = `Hi there ${name}!`;
+  }
+return newGreet;
+
 }
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
+    sayHello,
     // buildCar
 }
